99b095968a46ad75c3056afd05824afd501df338
ysjue <syang131@jh.edu>
2022-09-02 19:56:11 -0400

    evaluation real

--- evaluation_real.py
+++ evaluation_real.py
@@ -2,7 +2,9 @@ import os
 import numpy as np
 import scipy.stats as st
 import yaml
-from utils.transformations import quaternion_matrix, unit_vector
+from utils.transformations import rotation_matrix, quaternion_matrix, unit_vector
+from Solver.point_line_registration import homo
+from scipy.optimize import least_squares
 
 file_path = './data/testset1'
 trus_samples_txt = [f for f in os.listdir(file_path) if 'testset' in f]
@@ -51,8 +53,27 @@ cam_laser_start_spots = [cam2marker_transforms[i] @ np.array([ 49.9973,  18.979,
 cam_N = np.concatenate(cam_N,axis=1)
 cam_laser_start_spots = np.concatenate(cam_laser_start_spots,axis = 1)[:3]
 
+def fun(x,trus_spots,cam_laser_start_spots,cam_N,Freg):
+    theta = x[:1]
+    scale = x[1:]
+    laser_spots_pred = cam_laser_start_spots + scale * cam_N
+    rotms = [rotation_matrix(t*3.1415926/180.0,[1,0,0],[0,0,0]) for t in theta]
+    trus_spots = [ rotm[:3,:3] @ trus_spots[:,i][:,None]  \
+                    for i, rotm in zip(range(len(theta)), rotms)] 
+    trus_points = np.concatenate(trus_spots, axis = 1)
+    
+    error = np.sum(np.linalg.norm((Freg @ homo(trus_points))[:3] - laser_spots_pred, axis = 0)) #+ 0.1 *np.sum( theta**2)
+    return error
 
 
+Freg = np.array([[-5.02140697e-01,  8.64380456e-01 , 2.64792014e-02,  2.54306732e+01],
+ [ 8.51813669e-01 , 4.89092791e-01 , 1.87621201e-01, -1.59539019e+01],
+ [ 1.49225312e-01 , 1.16767586e-01 ,-9.81884483e-01 , 2.23744421e+02],
+ [ 0.00000000e+00,  0.00000000e+00 , 0.00000000e+00 , 1.00000000e+00]])
+result = least_squares(fun, [1,1],\
+                            bounds=([-5.0,2], [5.0,10] ),\
+                                args=(cam_laser_start_spots,cam_N,Freg))
+print(result)
 
 
     
